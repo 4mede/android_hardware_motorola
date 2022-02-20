1fed74fa9b76fbe07625199c129abfee5495117b
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    sensors: Implement UDFPS sensor
    Change-Id: I8b65e3196a7d6f158f4ff589b9755cf7c7e51c92

--- sensors/Sensor.h
+++ sensors/Sensor.h
@@ -148,6 +148,19 @@ class UdfpsSensor : public SysfsPollingOneShotSensor {
               static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 2)) {}
 };
 
+const std::string kTsUdfpsPressedPath = kTsPath + "udfps_pressed";
+const std::string kTsUdfpsEnabledPath = kTsPath + "udfps_enabled";
+
+class UdfpsSensor : public SysfsPollingOneShotSensor {
+  public:
+    UdfpsSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
+        : SysfsPollingOneShotSensor(
+                  sensorHandle, callback, kTsUdfpsPressedPath, kTsUdfpsEnabledPath, "UDFPS Sensor",
+                  "org.lineageos.sensor.udfps",
+                  static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) +
+                                          2)) {}
+};
+
 }  // namespace implementation
 }  // namespace subhal
 }  // namespace V2_1
