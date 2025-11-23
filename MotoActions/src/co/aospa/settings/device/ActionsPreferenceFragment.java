3a309a43540f003793c7c6c3a91043756b041188
Bruno Martins <bgcngm@gmail.com>
2025-11-23 21:14:35 +0000

    MotoActions: Convert for M3E support
    Change-Id: Ib4d1088f5be3fc2b5c2ff90d5cd1b86603551037

--- MotoActions/src/co/aospa/settings/device/ActionsPreferenceFragment.java
+++ MotoActions/src/co/aospa/settings/device/ActionsPreferenceFragment.java
@@ -20,9 +20,9 @@ package co.aospa.settings.device;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.preference.PreferenceFragmentCompat;
+import com.android.settingslib.widget.SettingsBasePreferenceFragment;
 
-public class ActionsPreferenceFragment extends PreferenceFragmentCompat {
+public class ActionsPreferenceFragment extends SettingsBasePreferenceFragment {
 
     private static final String KEY_ACTIONS_CATEGORY = "actions_key";
 
