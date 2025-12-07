d00398e2376782c9125b079b10e7275e25d3ed2a
Bruno Martins <bgcngm@gmail.com>
2025-12-07 10:08:32 +0000

    MotoActions: Replace Activity.MODE_PRIVATE with Context.MODE_PRIVATE
    Activity.MODE_PRIVATE is redundant because Activity is already a Context.
    
    Change-Id: Ib2e751de93294efe251fd5af272be27438b50025

--- MotoActions/src/co/aospa/settings/device/DozePreferenceFragment.java
+++ MotoActions/src/co/aospa/settings/device/DozePreferenceFragment.java
@@ -17,9 +17,9 @@
 
 package co.aospa.settings.device;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -44,7 +44,7 @@ public class DozePreferenceFragment extends PreferenceFragmentCompat
         setPreferencesFromResource(R.xml.doze_panel, rootKey);
 
         SharedPreferences prefs =
-                getActivity().getSharedPreferences("doze_panel", Activity.MODE_PRIVATE);
+                getActivity().getSharedPreferences("doze_panel", Context.MODE_PRIVATE);
         if (savedInstanceState == null && !prefs.getBoolean("first_help_shown", false)) {
             showHelp();
         }
@@ -110,7 +110,7 @@ public class DozePreferenceFragment extends PreferenceFragmentCompat
                 .setPositiveButton(R.string.dialog_ok,
                         (dialog, which) -> {
                             getActivity()
-                                    .getSharedPreferences("doze_panel", Activity.MODE_PRIVATE)
+                                    .getSharedPreferences("doze_panel", Context.MODE_PRIVATE)
                                     .edit()
                                     .putBoolean("first_help_shown", true)
                                     .commit();
