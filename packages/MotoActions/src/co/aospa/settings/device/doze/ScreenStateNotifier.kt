5e18108451ebd3d9ccf9c068ae255f98a78123f5
Nick Reuter <nreuter85@gmail.com>
2026-01-07 17:51:17 -0600

    MotoActions: Initial conversion to Kotlin
    Change-Id: Ia05e54e9a9ed096d6847760e5534a748ff550b36

---
+++ packages/MotoActions/src/co/aospa/settings/device/doze/ScreenStateNotifier.kt
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2015 The CyanogenMod Project
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.device.doze
+
+interface ScreenStateNotifier {
+    fun screenTurnedOn()
+
+    fun screenTurnedOff()
+}
